663d7d5f974058e6d788f2422ce8ea7357d9979c
Mudit Mehta <muditbudy@gmail.com>
2018-10-11 00:12:24 +0530

    ui first pahse

--- edudevs/dashboard/views.py
+++ edudevs/dashboard/views.py
@@ -2,8 +2,10 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
-
+@login_required(login_url='/$/')
 def dashboard(request):
     return render(request,'dashboard/dashboard.html')
